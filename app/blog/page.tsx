45ae7054f715d4920d631f5b51a933020214471d
Pedro Oliveira <pedro.carmo.oliveira@gmail.com>
2024-05-27 22:09:57 +0100

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,14 +1,13 @@
 import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
+  title: 'posts',
+  description: 'read my posts.',
 }
 
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
       <BlogPosts />
     </section>
   )
