0bbcf43eeef4c52e886a2a84400582d4d990434b
Pedro Oliveira <pedro.carmo.oliveira@gmail.com>
2024-05-28 18:48:02 +0100

    Add pfp to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+import Image from 'next/image'
 import { BlogPosts } from 'app/components/posts'
+import pfpImage from './pug.png'
 
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        gm 🫡
-      </h1>
+      <div className="flex items-center mb-8">
+        <Image
+          src={pfpImage}
+          alt="Your Name"
+          width={48}
+          height={48}
+          className="rounded-full mr-4"
+        />
+        <h1 className="text-2xl font-semibold tracking-tighter">gm 🫡</h1>
+      </div>
       <div className="my-8">
         <BlogPosts />
       </div>
