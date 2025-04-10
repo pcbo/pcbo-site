97fdc41a056c6194a7370f1891734ea199aff919
Pedro Oliveira <pedro.carmo.oliveira@gmail.com>
2025-04-10 11:55:37 +0100

    Cleaning up footer.

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,50 +1,9 @@
-function ArrowIcon() {
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
-      />
-    </svg>
-  )
-}
-
 export default function Footer() {
   return (
-    <footer className="mb-16">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://passport.talentprotocol.com/profile/1095"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">passport</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://warpcast.com/pcbo"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">farcaster</p>
-          </a>
-        </li>
-      </ul>
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        PCBO © {new Date().getFullYear()} All Rights Reserved
-      </p>
+    <footer className="w-full py-6 text-center text-sm text-gray-500 border-t border-gray-800 mt-12">
+      <div className="flex justify-center items-center">
+        © {new Date().getFullYear()} pcbo.xyz
+      </div>
     </footer>
-  )
+  );
 }
